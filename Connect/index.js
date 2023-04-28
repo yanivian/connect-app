fd1bb9c8a9b0312b32d1c4d266231f3d57f9e340
Vinay Papudesi <papudesi@hello.com>
2023-04-27 19:00:16 -0700

    Removes NavigationContainer.

--- Connect/index.js
+++ Connect/index.js
@@ -1,17 +1,14 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { AppRegistry } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import App from './App';
-import { name as appName } from './app.json';
+import { AppRegistry } from 'react-native'
+import { Provider as PaperProvider } from 'react-native-paper'
+import App from './App'
+import { name as appName } from './app.json'
 
 export default function Main() {
   return (
-    <NavigationContainer>
-      <PaperProvider>
-        <App />
-      </PaperProvider>
-    </NavigationContainer>
-  );
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  )
 }
 
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => Main)
