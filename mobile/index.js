e2415407f13b0eaf41a96d486c8582b764bd068d
Vinay Papudesi <papudesi@hello.com>
2023-05-08 15:16:50 -0700

    Imports react-native-url-polyfill/auto.

--- mobile/index.js
+++ mobile/index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native'
 import { Provider as PaperProvider } from 'react-native-paper'
+import 'react-native-url-polyfill/auto'
 import App from './App'
 import { name as appName } from './app.json'
 
